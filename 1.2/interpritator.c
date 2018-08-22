61af4f9f9a30cd0cc893e9184355e2ad5aad862c
malyavind <42367718+malyavind@users.noreply.github.com>
2018-08-22 16:31:38 +0700

    Update interpritator.c

--- 1.2/interpritator.c
+++ 1.2/interpritator.c
@@ -5,39 +5,39 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-int main (){
+int main(){
 	pid_t cpid;
-	while (1){
+		while(1){
 		cpid = fork();
-		if (cpid == 0){
+		
+		if (cpid == -1)
+			perror ("fork");
+		else if (cpid > 0){
+			///parent///
+			wait(0);
+		}
+		else if (!cpid){
 			int ret, j = 0;
 			char *str, *last;
 			char* args[100];
 			
 			fgets(str, 1024, stdin);
-			///_______________________________________spliting string into args
+			//printf("Splitting string %s into tokens:\n", str);
 			args[j] = strtok_r(str, " ", &last);
 			while (args[j] != NULL){
 				j++;
 				args[j] = strtok_r(NULL, " ", &last);
 			}
-			///_______________________________________spliting string into args
-			
+			args[j - 1][strlen(args[j - 1]) - 1] = '\0';
 			///________________________________________making path
 			char *start = "/bin/", path[50];
 			snprintf(path, sizeof path, "%s%s", start, args[0]);
 			///________________________________________making path
 			ret = execv (path, args);
 			if (ret == -1)
-				perror("execv");	
-		}	
-		else if (cpid > 0){
-			///parent///
-			wait(0);
-		}	
-		else if (cpid == -1)
-			perror ("fork");
-	}			
-				
+			perror("execv");
+		}
+	}
+
 	return 0;
-}	
+}
