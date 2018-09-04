a0f7320bb48bf062d28e7750f9e8a64a7c463a2f
malyavind <42367718+malyavind@users.noreply.github.com>
2018-09-05 02:20:19 +0700

    Create client.c

---
+++ 1.3/tcp/client.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+    int sock;
+    struct sockaddr_in addr;
+	char str[100];
+	
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+	
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(3425); // or another
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    
+    printf("This is client\n");
+    
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("connect");
+        exit(2);
+    }
+	while(1) {
+		fgets (str, 1024, stdin);
+		send(sock, str, sizeof(str), 0);
+		recv(sock, str, sizeof(str) , 0);
+		printf(str);
+	}	
+    close(sock);
+
+    return 0;
+}
