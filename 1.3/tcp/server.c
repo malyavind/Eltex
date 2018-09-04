dffeeaaaa0797e60f51ffd822fccbfd56c1e6838
malyavind <42367718+malyavind@users.noreply.github.com>
2018-09-05 02:23:48 +0700

    Update server.c

--- 1.3/tcp/server.c
+++ 1.3/tcp/server.c
@@ -7,32 +7,6 @@
 #include "mylib.h"
 #include "mylib.c"
 
-
-/*void * connection(void * arg) {
-	void* sock = (int) arg;
-	char buf[1024];
-	while(1) {
-		int bytes_read, bytes_sent = 0;
-		bytes_read = recv(sock, buf, sizeof(buf), 0);
-		if(bytes_read < 0) {
-			perror("recv");
-			exit (5);
-		}		
-		else if (bytes_read == 0) { break; }
-		buf[strlen(buf) - 1] = '1';
-		buf[strlen(buf)] = '\n';
-		while (bytes_sent < bytes_read) {
-			int current_bytes_sent = send(sock, buf+bytes_sent, bytes_read-bytes_sent, 0);
-			if (current_bytes_sent == -1) { goto disconn; }
-			bytes_sent += current_bytes_sent;
-		}
-	}
-	disconn:
-	close(sock);
-	printf("Client has disconnected from socket %ld\n", sock);
-	return NULL;
-}*/
-
 int main(void) { 
 	struct sockaddr_in addr;
 	int listener = socket(AF_INET, SOCK_STREAM, 0);
