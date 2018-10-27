009cb5ba5ac1708828146a4e3255b1a7ffc0fb4b
malyavind <42367718+malyavind@users.noreply.github.com>
2018-10-28 02:46:20 +0700

    Update receiver.c

--- broadcast_multicast/multicast/receiver.c
+++ broadcast_multicast/multicast/receiver.c
@@ -20,18 +20,18 @@ int main(void) {
 	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
 
-    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {        
+    	if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {        
 		perror("bind");
 		exit(2);
-    }    
-    mreq.imr_multiaddr.s_addr = inet_addr(GROUP);         
-    mreq.imr_interface.s_addr = htonl(INADDR_ANY);         
-    if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+ 	}    
+    	mreq.imr_multiaddr.s_addr = inet_addr(GROUP);         
+    	mreq.imr_interface.s_addr = htonl(INADDR_ANY);         
+    	if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
 		perror("setsockopt mreq");
 		exit(3);
 	}         
 	printf("THIS IS RECEIVER\n");
-    while (1) {
+   	while (1) {
 		cnt = recvfrom(sock, str, sizeof(str), 0, (struct sockaddr *) &addr, &addrlen);
 		if (cnt < 0) {
 			perror("recvfrom");
